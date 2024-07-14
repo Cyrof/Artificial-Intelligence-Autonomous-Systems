09e1b3335e053765efe2923bade1fd4c342c30b6
keith <keithneo00@gmail.com>
2024-07-15 03:43:54 +0800

    Updates - optimised cnn model (touch up required) - updated main.py to optimised dataloader.

--- assignment-2/code/alt_model.py
+++ assignment-2/code/alt_model.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.cuda.amp import GradScaler, autocast
 from tqdm import tqdm
 
 class ALTModel(nn.Module):
@@ -16,7 +17,7 @@ class ALTModel(nn.Module):
     def __init__(self):
         super(ALTModel, self).__init__()
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print(self.device)
+        # print(f"Using {self.device}")
         self.feature = nn.Sequential(
             # 1
             nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2), # 28*28-->32*32-->28*28
@@ -45,44 +46,108 @@ class ALTModel(nn.Module):
     def train_model(self, train_dataloader, test_dataloader, epochs, criterion, optimiser, accuracy):
         accuracy = accuracy.to(self.device)
         self = self.to(self.device)
-        for epoch in tqdm(range(epochs)):
+        scaler = GradScaler()
+
+        for epoch in range(epochs):
             train_loss, train_acc = 0.0, 0.0
             for x, y in train_dataloader:
-                x, y = x.to(self.device), y.to(self.device)
-                
-                self.train()
-                y_pred = self(x)
-                loss = criterion(y_pred, y)
-                train_loss += loss.item()
+                x, y =x.to(self.device), y.to(self.device)
 
-                acc = accuracy(y_pred, y)
-                train_acc += acc
-                
+                self.train()
                 optimiser.zero_grad()
-                loss.backward()
-                optimiser.step()
+
+                with autocast():
+                    y_pred = self(x)
+                    loss = criterion(y_pred, y)
+
+                scaler.scale(loss).backward()                    
+                scaler.step(optimiser)
+                scaler.update()
+
+                train_loss += loss.item()
+                train_acc += accuracy(y_pred, y)
 
             train_loss /= len(train_dataloader)
             train_acc /= len(train_dataloader)
 
-            # validation loop
+            # validation loop 
             val_loss, val_acc = 0.0, 0.0
             self.eval()
             with torch.inference_mode():
                 for x, y in test_dataloader:
                     x, y = x.to(self.device), y.to(self.device)
-                    
-                    y_pred = self(x)
-                    
-                    loss = criterion(y_pred, y)
-                    val_loss += loss.item()
-                    
-                    acc = accuracy(y_pred, y)
-                    val_acc += acc
 
+                    with autocast():
+                        y_pred = self(x)
+                        loss = criterion(y_pred, y)
+                    
+                    val_loss += loss.item()
+                    val_acc += accuracy(y_pred, y)
+                
                 val_loss /= len(test_dataloader)
                 val_acc /= len(test_dataloader)
             
-            print(f"Epoch: {epoch} | Train loss: {train_loss: .5f} | Train acc: {train_acc: .5f} | Val loss: {val_loss: .5f} | Val acc: {val_acc: .5f}")
+            print(f"Epoch: {epoch+1} | Train loss: {train_loss: .5f} | Train acc: {train_acc: .5f} | Val loss: {val_loss: .5f} | Val acc: {val_acc: .5f}")
+
+        # # for epoch in tqdm(range(epochs)):
+        # for epoch in range(epochs):
+        #     train_loss, train_acc = 0.0, 0.0
+        #     for x, y in train_dataloader:
+        #         x, y = x.to(self.device), y.to(self.device)
+                
+        #         self.train()
+        #         y_pred = self(x)
+        #         loss = criterion(y_pred, y)
+        #         train_loss += loss.item()
+
+        #         acc = accuracy(y_pred, y)
+        #         train_acc += acc
+                
+        #         optimiser.zero_grad()
+        #         loss.backward()
+        #         optimiser.step()
+
+        #     train_loss /= len(train_dataloader)
+        #     train_acc /= len(train_dataloader)
+
+        #     # validation loop
+        #     val_loss, val_acc = 0.0, 0.0
+        #     self.eval()
+        #     with torch.inference_mode():
+        #         for x, y in test_dataloader:
+        #             x, y = x.to(self.device), y.to(self.device)
+                    
+        #             y_pred = self(x)
+                    
+        #             loss = criterion(y_pred, y)
+        #             val_loss += loss.item()
+                    
+        #             acc = accuracy(y_pred, y)
+        #             val_acc += acc
+
+        #         val_loss /= len(test_dataloader)
+        #         val_acc /= len(test_dataloader)
             
+        #     print(f"Epoch: {epoch+1} | Train loss: {train_loss: .5f} | Train acc: {train_acc: .5f} | Val loss: {val_loss: .5f} | Val acc: {val_acc: .5f}")
+        
+    def test_model(self, test_dataloader, criterion, accuracy):
+        test_loss, test_acc = 0, 0
+
+        self.to(self.device)
+
+        self.eval()
+        with torch.inference_mode():
+            for x, y in test_dataloader:
+                x, y = x.to(self.device), y.to(self.device)
+                
+                y_pred = self(x)
+                
+                test_loss += criterion(y_pred, y)
+                test_acc += accuracy(y_pred, y)
+            
+            test_loss /= len(test_dataloader)
+            test_acc /= len(test_dataloader)
+        
+        print(f"Test loss: {test_loss: .5f}| Test acc:{test_acc: .5f}%")
+        return test_acc
         
