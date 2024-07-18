1f069a6f338adf3b8790909795f589ef1649c3b9
keith <keithneo00@gmail.com>
2024-07-19 02:04:28 +0800

    wrote internal documentation

--- assignment-2/code/analysis.py
+++ assignment-2/code/analysis.py
@@ -38,10 +38,6 @@ def altLoader():
     print(altD[0])
 
 if __name__ == "__main__":
-    # display_shapes()
-    # visualise_img()
-    # print(*x_train[0])
-    # altLoader()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(device)
     print(torch.cuda.is_available())
--- assignment-2/code/utils.py
+++ assignment-2/code/utils.py
@@ -6,6 +6,13 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, 
 import numpy as np
 
 def save_to_file(model, filename):
+    """
+    Saves a trained model object to a pickle file.
+
+    Parameters:
+    model (object): Trained model object to be saved.
+    filename (str): Name of the pickle file to save the model to.
+    """
     dir_path = f"{os.path.dirname(os.path.realpath(__file__))}/models"
     path = f"{dir_path}/{filename}.pkl"
 
@@ -20,6 +27,15 @@ def save_to_file(model, filename):
     print(f"Model saved to {path}")
 
 def import_model(filename):
+    """
+    Load a trained model object from a pickle file.
+
+    Parameters: 
+    filename (str): Name of the file from which to load the model. 
+
+    Returns: 
+    object: Loaded model object.
+    """
     path = f"{os.path.dirname(os.path.realpath(__file__))}/models/{filename}.pkl"
     with open(path, 'rb') as f:
         model = pickle.load(f)
@@ -28,10 +44,18 @@ def import_model(filename):
     return model    
 
 def plot_confusion_matrix(cm, model_name):
+    """
+    Plots and saves the confusion matrix.
+
+    Parameters: 
+    cm (ndarray): Confusion matrix array.
+    model_name (str): Name of the model for which the confusion matrix is being plotted.
+    """
     dir_path = f"{os.path.dirname(os.path.realpath(__file__))}/plots"
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
+    # Display confusion matrix using sklearn's ConfusionMatrixDisplay
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=range(10))
     disp.plot(colorbar=False)
     plt.title(f"{model_name} Confusion Matrix")
@@ -39,6 +63,15 @@ def plot_confusion_matrix(cm, model_name):
     print(f"Confusion matrix saved to {dir_path}/{model_name}_confusion_matrix.png\n")
 
 def plot_roc_curve(y_true, y_scores, model_name, num_classes):
+    """
+    Plots and saves ROC curves for multi-class classfication.
+
+    Parameters: 
+    y_true (ndarray): True labels.
+    y_scores (ndarray): Predicted scores.
+    model_name (str): Name of the model for which the ROC curves are being plotted.
+    num_classes (int): Number of classes in the dataset.
+    """
     dir_path = f"{os.path.dirname(os.path.realpath(__file__))}/plots"
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
@@ -74,6 +107,16 @@ def plot_roc_curve(y_true, y_scores, model_name, num_classes):
 
 
 def plot_class_wise_metrics(precision, recall, f1_score, model_name, num_classes):
+    """
+    Plots and saves class-wise precision, recall, and F1 score metrics.
+
+    Parameters: 
+    precision (ndarray): Precision scores.
+    recall (ndarray): Recall scores.
+    f1_score (ndarray): F1 scores.
+    model_name (str): Name of the model for which the metrics are being plotted.
+    num_classes (int): Number of classes in the dataset.
+    """
     dir_path = f"{os.path.dirname(os.path.realpath(__file__))}/plots"
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
@@ -81,6 +124,7 @@ def plot_class_wise_metrics(precision, recall, f1_score, model_name, num_classes
     x = range(num_classes)
     width = 0.2
 
+    # plotting bar chart for precision, recall, and F1 score
     plt.figure(figsize=(12, 6))
     plt.bar(x, precision, width, label="Precision")
     plt.bar([p + width for p in x], recall, width, label="Recall")
@@ -96,6 +140,15 @@ def plot_class_wise_metrics(precision, recall, f1_score, model_name, num_classes
     print(f"Class-wise performance metrics saved to {dir_path}/{model_name}_Class_wise_metrics.png\n")
 
 def plot_precision_recall_curve(y_true, y_scores, model_name, num_classes):
+    """
+    Plots and saves precision-recall curves for multi-class classification.
+
+    Parameters: 
+    y_true (ndarray): True labels.
+    y_scores (ndarray): Predicted scores.
+    model_name (str): Name of the model for which the precision-recall curves are being plotted.
+    num_classes (int): Number of classes in the dataset.
+    """
     dir_path = f"{os.path.dirname(os.path.realpath(__file__))}/plots"
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
